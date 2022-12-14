e5173de7c551a465b85be3a3bd5318dbb5a83826
Makayla Anderson-Tucker <makayla.anderson.tucker@gmail.com>
2022-12-14 13:43:07 -0800

    Designed the nav bar. Will add destination links later.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,33 @@
 
 import './App.css';
+import icon from "./images/talent-icon.svg";
 
-function App() {
+const Nav = () => {
+  
+  return (
+    <div className="nav-bar">
+      
+        <img src={icon} width={190} height={60}/>
+     
+      <div className="nav-items">
+        <div className="item">
+          <h3><a href="#">Contact</a></h3>
+        </div>
+        <div className="item">
+          <h3><a href="#">About</a></h3>
+        </div>
+        <div className="item">
+          <h3><a href="#">Tax Calculator</a></h3>
+        </div>
+      </div>
+    </div>
+  )
+};
+
+const App = () => {
   return (
     <div className="App">
-    <h1>Income Tax Calculator</h1>
+      <Nav />
     </div>
   );
 }
